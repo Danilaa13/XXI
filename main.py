184ede02d72fc1c768ad48f1a38415ddbadcbb34
Danilaa13 <dk.prozak@gmail.com>
2025-04-14 22:54:21 +0300

    Добавление текстур

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import os
 
-# Читаем JSON-файл
+
 with open('products_new.json', 'r', encoding='utf-8') as file:
     data = pd.read_json(file)
 
@@ -17,6 +18,19 @@ cost_index = columns.index('Стоимость')
 columns.remove('Стоимость')
 columns.insert(thickness_index + 1, 'Стоимость')
 data = data[columns]
+
+# Папка с изображениями
+image_folder = r"E:\pythonProjectsForUniversity\WORK\PARS_PICTURES\XXI\XXI_images"
+
+def find_image_by_article(row):
+    global image_count
+    article = row['Артикул материала']
+    for file in os.listdir(image_folder):
+        if article in file and file.lower().endswith(('.png', '.jpg', '.jpeg')):
+            return f"KRONOSPAN\\{file}"
+    return "Фото не найдено"
+
+data['Текстура'] = data.apply(find_image_by_article, axis=1)
 data.to_excel('XXI_new.xlsx', index=False, engine='openpyxl')
 
 print("Файл успешно сохранён как XXI_new.xlsx")
